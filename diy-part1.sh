4f9425979abddf7dfaf1b1412b46de979843b2d1
Wensheng Tang <sp3478@gmail.com>
2020-09-25 00:57:17 +0800

    Revert "add acme"
    This reverts commit 5c7c67e0ae693db8ec459068199ce63d2e095d37.

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git luci2 https://github.com/openwrt/luci' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
